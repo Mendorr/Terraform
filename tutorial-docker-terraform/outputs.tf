cea38595c5286b45ff97d928ab15f739ebaa572e
awahiid <awmahfoud@proton.me>
2026-01-14 15:15:21 +0100

    Eliminados ficheros inutilizados

--- tutorial-docker-terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-//4. Archivo de variables de salida
-// Útil cuando se quiere interoperabilidad
-// con otros servicios.
-// Las variables de salida almacenan información de la infraestructura
-// cuando se ejecuta apply
-
-// Estos valores se almacenan en el estado de Terraform.
-// Se pueden ver con
-// $ terraform output
